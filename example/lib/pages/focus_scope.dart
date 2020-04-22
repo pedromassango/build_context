8393386e83a3d4d34b136059b0855b7ddc8d9888
Victor Uvarov <victoruvarov23@gmail.com>
2020-04-22 00:14:11 -0700

    Add focus scope extension
    - add focus scope docs
    - updgrade example to android x
    - add focus scope to example
    - add form to example

---
+++ example/lib/pages/focus_scope.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:build_context/build_context.dart';
+
+class FocusScopePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: context.focusScope.unfocus,
+      excludeFromSemantics: true,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Focus'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              autofocus: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Click away from me...',
+                hintStyle: context.primaryTextTheme.subhead
+                    .copyWith(color: context.primaryColor),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
