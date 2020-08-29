79c73aad10ac3b73aeb29c79e84c84234276ab2d
Kauê Martins <ksampaiosp@gmail.com>
2020-08-29 11:45:34 -0300

    Add textstyle extension

--- lib/src/build_context_impl.dart
+++ lib/src/build_context_impl.dart
@@ -88,6 +88,32 @@ extension ThemeExt on BuildContext {
   bool get isFuchsia => this.platform == TargetPlatform.fuchsia;
 
   bool get isLinux => this.platform == TargetPlatform.linux;
+
+  TextStyle get headline1 => textTheme.headline1;
+
+  TextStyle get headline2 => textTheme.headline2;
+
+  TextStyle get headline3 => textTheme.headline3;
+
+  TextStyle get headline4 => textTheme.headline4;
+
+  TextStyle get headline5 => textTheme.headline5;
+
+  TextStyle get headline6 => textTheme.headline6;
+
+  TextStyle get subtitle1 => textTheme.subtitle1;
+
+  TextStyle get bodyText1 => textTheme.bodyText1;
+
+  TextStyle get bodyText2 => textTheme.bodyText2;
+
+  TextStyle get caption => textTheme.caption;
+
+  TextStyle get button => textTheme.button;
+
+  TextStyle get subtitle2 => textTheme.subtitle2;
+
+  TextStyle get overline => textTheme.overline;
 }
 
 extension ScaffoldExt on BuildContext {
