4a1baf0bf671f23803002536931c12d3a883aae1
nilsreichardt <nils@sharezone.net>
2021-03-07 15:22:45 +0100

    Migrated to new text theme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white24,
         primaryTextTheme: TextTheme(
-          title: TextStyle(color: Colors.blue),
+          headline6: TextStyle(color: Colors.blue),
         ),
       ),
       home: HomePage(),
@@ -46,14 +46,14 @@ class HomePage extends StatelessWidget {
                 onPressed: () => context.pushNamed(Routes.form),
                 child: Text(
                   'Form Example',
-                  style: context.primaryTextTheme.title,
+                  style: context.primaryTextTheme.headline6,
                 ),
               ),
               MaterialButton(
                 onPressed: () => context.pushNamed(Routes.focusScope),
                 child: Text(
                   'Focus Scope Example',
-                  style: context.primaryTextTheme.title,
+                  style: context.primaryTextTheme.headline6,
                 ),
               ),
             ],
